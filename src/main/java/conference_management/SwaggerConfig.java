8f0a1b75ab0c01738018545e676427ebc8d4f3a9
mariakax3 <marysiakaraszewska@gmail.com>
2023-05-15 10:31:48 +0200

    Added swagger

---
+++ src/main/java/conference_management/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package conference_management;
+
+import org.springdoc.core.properties.SpringDocConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springdoc.core.providers.SpringWebProvider;
+import org.springdoc.webmvc.ui.SwaggerWelcomeWebMvc;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(
+            name = {"springdoc.use-management-port"},
+            havingValue = "false",
+            matchIfMissing = true
+    )
+    SwaggerWelcomeWebMvc swaggerWelcome(SwaggerUiConfigProperties swaggerUiConfig,
+                                        SpringDocConfigProperties springDocConfigProperties,
+                                        SwaggerUiConfigParameters swaggerUiConfigParameters,
+                                        SpringWebProvider springWebProvider
+    ) {
+        return new SwaggerWelcomeWebMvc(swaggerUiConfig, springDocConfigProperties, swaggerUiConfigParameters, springWebProvider);
+    }
+}
